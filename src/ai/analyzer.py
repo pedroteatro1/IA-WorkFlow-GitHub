45c865faed7ed0c345edd4ed64279a314690fe6d
Pedro Henrique <pedroteatro1@gmail.com>
2026-07-11 17:59:51 -0300

    Melhora lógica de análise de sentimento

--- src/ai/analyzer.py
+++ src/ai/analyzer.py
@@ -1,5 +1,4 @@
 def analisar_texto(texto):
-
     palavras_positivas = [
         "feliz",
         "ótimo",
@@ -18,12 +17,21 @@ def analisar_texto(texto):
 
     texto = texto.lower()
 
+    pontos = 0
+
     for palavra in palavras_positivas:
         if palavra in texto:
-            return "Sentimento positivo ❤️"
+            pontos += 1
 
     for palavra in palavras_negativas:
         if palavra in texto:
-            return "Sentimento negativo 😞"
+            pontos -= 1
 
-    return "Neutro 😐"
+    if pontos > 0:
+        return "Sentimento positivo ❤️"
+
+    elif pontos < 0:
+        return "Sentimento negativo 😞"
+
+    else:
+        return "Neutro 😐"
