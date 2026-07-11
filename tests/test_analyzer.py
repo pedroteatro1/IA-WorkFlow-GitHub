3573c16d340b88b0a07d78f82d66553117a1bd5a
Pedro Henrique <pedroteatro1@gmail.com>
2026-07-11 18:07:22 -0300

    Adiciona teste de sentimento neutro

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -9,3 +9,8 @@ def test_sentimento_positivo():
 def test_sentimento_negativo():
     resultado = analisar_texto("Esse projeto está ruim")
     assert resultado == "Sentimento negativo 😞"
+
+
+def test_sentimento_neutro():
+    resultado = analisar_texto("Hoje está um dia comum")
+    assert resultado == "Neutro 😐"
